9baa76e2712a8fd7772c29c6c4e9725cdd47638b
rasmusmerzin <rasmusmerzin@gmail.com>
2020-04-24 20:01:14 +0300

    Updates

---
+++ precache-manifest.54a045adf022bc2b0503687d01e458d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cf96bae419abe27f30c3125c1511fa3",
+    "url": "/ants/index.html"
+  },
+  {
+    "revision": "f03a6725d211a2be2abb",
+    "url": "/ants/static/css/main.32e8028d.chunk.css"
+  },
+  {
+    "revision": "fe70c21d14e4c21b165b",
+    "url": "/ants/static/js/2.a198882a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ants/static/js/2.a198882a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f03a6725d211a2be2abb",
+    "url": "/ants/static/js/main.36c0ec01.chunk.js"
+  },
+  {
+    "revision": "a10f0e37dc2be9dc7b0e",
+    "url": "/ants/static/js/runtime-main.73b50742.js"
+  }
+]);
